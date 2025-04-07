121a740c88128bed77bde51d98f196317a1e94d1
JPimia <90615229+JPimia@users.noreply.github.com>
2025-04-07 17:12:42 +0300

    Add Game struct and core logic for handling game state, input, collision detection, and food spawning

---
+++ src/game.rs
@@ -0,0 +1,87 @@
+use piston_window::*;
+use rand::{rng, Rng};
+use crate::snake::{Snake, Direction};
+
+pub static mut HIGHSCORE: i32 = 0;
+
+pub struct Game {
+    pub snake: Snake,
+    pub food: Vec<(i32,i32)>,
+    pub width: i32,
+    pub height: i32,
+    pub score: i32,
+    pub game_over: bool
+}
+
+impl Game {
+    pub fn new(width: i32, height: i32) -> Self {
+        let rng = rng().random_range(0..width);
+        Game {
+            snake: Snake {
+                body: vec![(width / 2, height / 2)],
+                direction: Direction::Right,
+            },
+            food: vec![(rng, rng)],
+            width,
+            height,
+            score: 0,
+            game_over: false
+        }
+    }
+
+    pub fn handle_input(&mut self, key: Key) {
+        match key {
+            Key::W => if self.snake.direction != Direction::Down { self.snake.direction = Direction::Up },
+            Key::D => if self.snake.direction != Direction::Left { self.snake.direction = Direction::Right },
+            Key::S => if self.snake.direction != Direction::Up { self.snake.direction = Direction::Down },
+            Key::A => if self.snake.direction != Direction::Right { self.snake.direction = Direction::Left },
+            _ => {},
+        }
+    }
+
+    pub fn check_collusion(&mut self) {
+        let head = self.snake.body[0];
+
+        if head.0 < 0 || head.1 > self.width - 1 || head.0 > self.height - 1 || head.1 < 0 {
+            self.game_over = true;
+        }
+        if self.snake.body.iter().skip(1).any(|segment| segment == &head) {
+            self.game_over = true;
+        }
+    }
+
+    pub fn spawn_food(&mut self) {
+        let &last_pos = self.snake.body.last().unwrap();
+        let head = self.snake.body[0];
+        let (x, y) = last_pos;
+
+        if self.food[0] == head {
+            loop { 
+                let new_food = (rng().random_range(0..self.width), rng().random_range(0..self.height));
+                if !self.snake.body.iter().any(|segment| segment == &new_food) {
+                    self.food.pop();
+                    self.food.push(new_food);
+
+                    let len = self.snake.body.len();
+                    if len >= 2 {
+                        let tail = self.snake.body[len - 1];
+                        let previous_seg = self.snake.body[len - 2];
+                        let dx = tail.0 - previous_seg.0;
+                        let dy = tail.1 - previous_seg.1;
+                        let new_seg = (tail.0 + dx, tail.1 + dy);
+                        self.snake.body.push(new_seg);
+                    } else {
+                        match self.snake.direction {
+                            Direction::Up => self.snake.body.push((x, y + 1)),
+                            Direction::Down => self.snake.body.push((x, y - 1)),
+                            Direction::Right => self.snake.body.push((x - 1, y)),
+                            Direction::Left => self.snake.body.push((x + 1, y)),
+                        }
+                    }
+                    self.score += 1;
+                    break;
+                }
+            }
+        }
+    }
+} 
